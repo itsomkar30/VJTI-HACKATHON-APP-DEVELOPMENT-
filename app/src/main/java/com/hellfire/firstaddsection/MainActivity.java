de5252ab1a6c734d3d8d5c226984feca019e730a
Omkar Prabhudesai <itsomkar30@gmail.com>
2023-02-19 03:35:16 +0530

    First aid (training) module with 5 basic first aid treatments

---
+++ app/src/main/java/com/hellfire/firstaddsection/MainActivity.java
@@ -0,0 +1,69 @@
+package com.hellfire.firstaddsection;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN |
+                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
+        Button button1 = findViewById(R.id.button);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, DogBite.class);
+                startActivity(intent);
+            }
+        });
+
+
+        Button button2 = findViewById(R.id.button1);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v1) {
+                Intent intent1 = new Intent(MainActivity.this, SnakeBite.class);
+                startActivity(intent1);
+            }
+        });
+
+        Button button3 = findViewById(R.id.button2);
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v2) {
+                Intent intent2 = new Intent(MainActivity.this, Skinburn.class);
+                startActivity(intent2);
+            }
+        });
+
+        Button button4 = findViewById(R.id.button3);
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v3) {
+                Intent intent3 = new Intent(MainActivity.this, ViralInfection.class);
+                startActivity(intent3);
+            }
+        });
+
+        Button button5 = findViewById(R.id.button4);
+        button5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v4) {
+                Intent intent4 = new Intent(MainActivity.this, ViralInfection.class);
+                startActivity(intent4);
+            }
+        });
+    }
+}
